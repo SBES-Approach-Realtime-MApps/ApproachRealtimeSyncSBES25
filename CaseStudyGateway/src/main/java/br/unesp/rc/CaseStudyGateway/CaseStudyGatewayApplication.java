985d0912546965c01f8a7cedcab582d895714c51
DanielAlmeida19 <dani1114vivilinda@gmail.com>
2025-05-06 11:35:58 -0300

    refactor: an api gateway to stardardize URLs

---
+++ CaseStudyGateway/src/main/java/br/unesp/rc/CaseStudyGateway/CaseStudyGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.unesp.rc.casestudygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseStudyGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseStudyGatewayApplication.class, args);
+	}
+
+}
